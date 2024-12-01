c85cc4dfd1948abe231f4c92ac1d26dac5230cc4
pavlovw <133174113+pavlovw@users.noreply.github.com>
2024-12-01 04:33:03 +0000

    Mensaje explicativo sobre los cambios

--- sitiolex/settings.py
+++ sitiolex/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sitiolex.urls'
@@ -134,6 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
