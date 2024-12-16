f71694c4864a095f20b38dcfffb00588cfb8bc48
pavlovw <133174113+pavlovw@users.noreply.github.com>
2024-12-16 05:39:41 +0000

    saac

--- sitiolex/settings.py
+++ sitiolex/settings.py
@@ -183,6 +183,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # myproject/settings.py
-
+LOGIN_URL = '/account/login/'
 AUTH_USER_MODEL = 'account.CustomUser'
 
